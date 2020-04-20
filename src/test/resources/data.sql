13b81a55c23f9399bec869432eb6180acc809c25
Sreenivasa Rao <sreenivasrao.m@autorabit.com>
2020-04-20 23:59:10 +0530

    Fixed mysql alter table issue.

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO user (id, first_name, last_name, email, password) VALUES (1, 'Memory', 'Not Found', 'info@memorynotfound.com', '$2a$10$RyY4bXtV3LKkDCutlUTYDOKd2AiJYZGp4Y7MPVdLzWzT1RX.JRZyG');
+INSERT INTO user (id, first_name, last_name, email, password) VALUES (1, 'Memory', 'Not Found', 'sree.muppavarapu@gmail.com', '$2a$10$RyY4bXtV3LKkDCutlUTYDOKd2AiJYZGp4Y7MPVdLzWzT1RX.JRZyG');
 
 INSERT INTO role (id, name) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO role (id, name) VALUES (2, 'ROLE_MANAGER');
